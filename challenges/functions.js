df061ca18109b73ec21f7a6ae6a171338a8f80ec
Gregory Hawman <g.hawman@gmail.com>
2020-01-31 09:54:05 -0800

    finished prototypes!!!

--- challenges/functions.js
+++ challenges/functions.js
@@ -41,7 +41,7 @@ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice t
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: You inherit genes from your parents, they can't get any genes back from you. Closures work like that
+// Explanation: You inherit genes from your parents, they can't get any genes back from you. Closures work like that.
 
 
 
