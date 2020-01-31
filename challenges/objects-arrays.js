49892551ca36bc0b029b08546cb21ffe40239256
Gregory Hawman <g.hawman@gmail.com>
2020-01-31 10:09:46 -0800

    finished class!!! going to go back over everything and make sure I did it right.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -91,12 +91,14 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
+
 const contactInfo = graduates.map((uni) => {
   return `${uni.first_name} ${uni.email}`;
 });
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
+
 const unisWithUni = graduates.filter((uni) => {
   return uni.university.toLowerCase().indexOf('uni'.toLowerCase()) !== -1 ;
 });
@@ -125,10 +127,13 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
+
 const displayNames = zooAnimals.forEach((names) => {
   console.log(`Name: ${names.animal_name}, Scientific: ${names.scientific_name}.`)
 });
 console.log(displayNames);
+
+
 
 /* Request 2: .map()
 
@@ -136,14 +141,12 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-// const unisWithUni = graduates.filter(function(uni){
-//   return uni.university.toLowerCase().indexOf('uni'.toLowerCase()) !== -1 ;
-// });
-
 const lowCaseAnimalNames = zooAnimals.map((names) => {
   return names.animal_name.toLowerCase();
 });
 console.log(lowCaseAnimalNames);
+
+
 
 /* Request 3: .filter() 
 
@@ -151,11 +154,12 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 
-
 const lowPopulationAnimals = zooAnimals.filter((pop) => {
   return pop.population < 5;
 });
 console.log(lowPopulationAnimals);
+
+
 
 /* Request 4: .reduce() 
 
